32c4a4fd403ebd0be65af328b3a92223214ca32e
naderscode <naderscode@gmail.com>
2019-01-15 20:31:11 -0500

    configure the SASS task in the Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,21 @@
 
 module.exports = function (grunt) {
   //define configuiration for tasks
+
+  // Time how long tasks take. Can help when optimizing build times
+   require('time-grunt')(grunt);
+
+   // Automatically load required Grunt tasks
+   require('jit-grunt')(grunt);
+
   grunt.initConfig({
-    
+    sass: {
+           dist: {
+               files: {
+                   'css/styles.css': 'css/styles.scss'
+               }
+           }
+       }
   });
+  grunt.registerTask('css', ['sass']);
 };
