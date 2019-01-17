60ffbfc7252794e4837e075dc4506263592f544c
naderscode <naderscode@gmail.com>
2019-01-16 21:51:03 -0500

    add copy and clean code to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,36 @@ module.exports = function (grunt) {
                     }
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [
+                {
+                    //for html
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [
+                {
+                    //for font-awesome
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+
+        clean: {
+            build: {
+                src: [ 'dist/']
+            }
         }
   });
   grunt.registerTask('css', ['sass']);
